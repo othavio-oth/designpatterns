ebe3aca1e0d538f657ef017b80b105aa620020e6
Othavio <otaviodelimasousa@gmail.com>
2024-04-08 18:15:03 -0300

    sysout in person

--- src/main/java/com/othavio/creational/builder/Main.java
+++ src/main/java/com/othavio/creational/builder/Main.java
@@ -1,5 +1,7 @@
 package com.othavio.creational.builder;
 
+import java.time.LocalDate;
+
 import com.othavio.creational.builder.builders.PersonBuilder;
 import com.othavio.creational.builder.model.Person;
 
@@ -7,7 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Person person = new PersonBuilder().address("123 Main Street").name("John Doe").build();
+        Person person = new PersonBuilder().name("John Doe")
+                .birthDate(LocalDate.of(1990, 1, 1))
+                .phoneNumber("555-555-5555")
+                .email("myemail@me.com")
+                .address("123 Main Street")
+                .build();
         System.out.println(person);
 
     }
